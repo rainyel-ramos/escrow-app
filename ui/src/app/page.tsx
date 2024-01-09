17fa8f7125132ab8568646ccab65be29175bb1b5
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-01-08 19:01:46 -0500

    add base code for ui

---
+++ ui/src/app/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import { Col, Container, Row } from 'react-bootstrap'
+
+
+export default function Home() {
+  return (
+    <main>
+      <Container>
+        <h1>Escrow App</h1>
+        <Row>
+          <Col>
+          </Col>
+          <Col>
+          </Col>
+        </Row>
+      </Container>
+    </main>
+  )
+}
