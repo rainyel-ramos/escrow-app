7b7b1e64ebc6b17f4b133586d1af46d1a65320d6
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-01-09 17:57:04 -0500

    rename to escrow schema

--- ui/src/app/schemas/formSchemas/escrowSchema.ts
+++ ui/src/app/schemas/formSchemas/escrowSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { AddressSchema } from "../inputSchemas/addressSchema";
 
-export const VerifierSchema = z.object({
+export const EscrowSchema = z.object({
     arbiter: AddressSchema, // renders TextField
     beneficiary: AddressSchema,
     amount: z.number(), // renders TextField
