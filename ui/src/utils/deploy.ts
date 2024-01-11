743f093a1334d4a6e3a2d0036e77db86c89de02e
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-01-11 15:39:15 -0500

    fix with await deployment

--- ui/src/utils/deploy.ts
+++ ui/src/utils/deploy.ts
@@ -1,11 +1,13 @@
 import { ethers } from 'ethers';
 import Escrow from '@/artifacts/contracts/Escrow.sol/Escrow.json';
+import { Escrow__factory } from '@/typechain';
 
 export default async function deploy(signer: any, arbiter: any, beneficiary: any, value: any) {
   const factory = new ethers.ContractFactory(
     Escrow.abi,
     Escrow.bytecode,
     signer
-  );
-  return factory.deploy(arbiter, beneficiary, { value });
+  ) as Escrow__factory;
+  const deployTx = await factory.deploy(arbiter, beneficiary, { value });
+  return deployTx.waitForDeployment(); 
 }
