c0422a400e38dc1ed60955ec46ad5d6676e827c9
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-01-11 15:39:00 -0500

    add typechain config to export types to ui

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,10 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  typechain: {
+    outDir: "./ui/src/typechain",
+    target: "ethers-v6",
+  },
   paths: {
     artifacts: "./ui/src/artifacts",
   }
