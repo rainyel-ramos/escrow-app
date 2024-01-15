b0a75f4867ae73d5a343003c1cf026fbe7d53d8c
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-01-15 18:16:04 -0500

    change template to bootstrap

--- ui/src/app/components/Escrow.tsx
+++ ui/src/app/components/Escrow.tsx
@@ -3,22 +3,23 @@ import { approve } from '@/utils/randomUtils';
 import { BasicEscrow } from '@/utils/types';
 import { useContractEvent } from 'wagmi';
 import EscrowAbi from '@/artifacts/contracts/Escrow.sol/Escrow.json';
+import { Button, ListGroup } from 'react-bootstrap';
 
 
 export interface EscrowProps {
     escrowProperties: BasicEscrow;
 }
 
-export default function Escrow({escrowProperties}: EscrowProps) {
+export default function Escrow({ escrowProperties }: EscrowProps) {
     const signer = useEthersSigner();
     const unwatch = useContractEvent({
         address: escrowProperties.address,
         abi: EscrowAbi.abi,
         eventName: 'Approved',
         listener(log) {
-          console.log(log)
-          //TODO - here change the status of the contract component
-          unwatch?.();
+            console.log(log)
+            //TODO - here change the status of the contract component
+            unwatch?.();
         },
     })
     async function handleApprove() {
@@ -26,32 +27,11 @@ export default function Escrow({escrowProperties}: EscrowProps) {
     }
 
     return (
-        <div className="existing-contract">
-            <ul className="fields">
-                <li>
-                    <div> Arbiter </div>
-                    <div> {escrowProperties.arbiter} </div>
-                </li>
-                <li>
-                    <div> Beneficiary </div>
-                    <div> {escrowProperties.beneficiary} </div>
-                </li>
-                <li>
-                    <div> Value </div>
-                    <div> {escrowProperties.value} </div>
-                </li>
-                <div
-                    className="button"
-                    id={escrowProperties.address}
-                    onClick={(e) => {
-                        e.preventDefault();
-
-                        handleApprove();
-                    }}
-                >
-                    Approve
-                </div>
-            </ul>
-        </div>
+        <ListGroup>
+            <ListGroup.Item>Arbiter: {escrowProperties.arbiter}</ListGroup.Item>
+            <ListGroup.Item>Beneficiary: {escrowProperties.beneficiary}</ListGroup.Item>
+            <ListGroup.Item>Value: {escrowProperties.value}</ListGroup.Item>
+            <Button variant="primary" onClick={handleApprove}>Approve</Button>
+        </ListGroup>
     );
 }
