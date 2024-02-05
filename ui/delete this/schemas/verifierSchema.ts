24429d1bbdcda3ff96a1842ae151837b62905b8f
Rainyel Ramos <rainyel.ramos@gmail.com>
2024-02-05 16:22:12 -0500

    delete not necessary code

--- ui/delete this/schemas/verifierSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-import { AddressSchema } from "../schemas/addressSchema";
-
-export const VerifierSchema = z.object({
-    contractAddress: AddressSchema, // renders TextField
-    blockNumber: z.number(),
-    contractCode: z.string(), // renders TextField
-});
